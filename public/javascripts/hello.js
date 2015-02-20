31ec25293bd8e30423e0133681f435ff3a67a766
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2015-02-20 20:39:49 +0200

    patients data

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,6 +1,5 @@
 var app = angular.module("app", ["ngResource", "ngRoute"])
     .constant("apiUrl", "mopital.herokuapp.com/api")
-    .constant("appUrl", "mopital.herokuapp.com")
     .config(function ( $httpProvider) {
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
